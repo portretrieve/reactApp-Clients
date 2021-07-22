7309fb61df0766e6ba848e95d673bcfb819fa5ce
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-07-22 19:47:28 -0400

    updated RTMP server

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,17 +1,54 @@
 import React from "react";
 import Navbar from "./Navbar";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream_AC } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream_AC(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream_AC(id);
+    this.createPlayer();
+  }
+
+  componentDidUpdate() {
+    this.createPlayer();
+  }
+
+  componentWillUnmount() {
+    console.log("unmounted");
+    this.player.destroy();
+  }
+
+  createPlayer() {
+    const { id } = this.props.match.params;
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: "http://localhost:8000/live/" + id + ".flv"
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
         <Navbar title={"Show"}></Navbar>
+        <video ref={this.videoRef} style={{ width: "60vw" }} controls />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
